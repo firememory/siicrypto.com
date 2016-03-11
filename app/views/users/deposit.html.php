f97a59f883ee5135a62d79453a6c9db98dcfe095
Nilam Doctor <nilamdoc@gmail.com>
2016-03-11 20:26:26 +0530

    deposit page

--- app/views/users/deposit.html.php
+++ app/views/users/deposit.html.php
@@ -2,10 +2,8 @@
 <h2>Deposit <?=$data['data']['currency']?></h2>
 <p>You have requested to deposit <?=$data['data']['amountFiat']?> <?=$data['data']['currency']?> to your SiiCrypto.com account <strong><?=$data['username']?></strong></p>
 <p>Click to download the PDF directly from here <a href="/vanity/out/SiiCrypto-<?=$data['data']['Reference']?>.pdf" target="_blank">SiiCrypto-<?=$data['data']['Reference']?>.pdf</a></p>
-<p>
-Print and Sign the PDF document. You will have to upload the signed document <a href="/users/funding_fiat/<?=$data['data']['currency']?>">here</a>.
+<p>Print, Sign and Upload the PDF document.</p>
 Once you upload "<strong>Declaration of Source of Funds (DSF)</strong>", we will send this document for SiiCrypto Admin Approval. Once it is approved, you can send the funds to the account.</p>
-<p>SiiCrypto confirms receipt of your Declaration of Source of Funds. Your document is being processed and you will receive a further email shortly.</p>
 <a href="/users/sendDeposit/<?=$data['data']['Reference']?>" class="btn btn-primary">Click to send to directly to your email to sign it</a>
 <p>&nbsp;</p>
 <a href="/ex/dashboard" class="btn btn-primary">Dashboard</a>
