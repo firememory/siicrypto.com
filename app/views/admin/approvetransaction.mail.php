9aed6eeb92a801009abcd5d53b8d4876319e574e
Nilam Doctor <nilamdoc@gmail.com>
2016-03-11 06:09:24 +0530

    Approve

--- app/views/admin/approvetransaction.mail.php
+++ app/views/admin/approvetransaction.mail.php
@@ -1,10 +1,47 @@
-<div style="background-color:#eeeeee;height:50px;padding-left:20px;padding-top:10px">
-	<img src="https://<?=COMPANY_URL?>/img/<?=COMPANY_URL?>.gif" alt="<?=COMPANY_URL?>">
-</div>
-<h4>Hi <?=$user['firstname']?>,</h4>
-
-<p>Your deposit has been approved <?=COMPANY_URL?>.</p>
-<table>
+<?php
+			$response = file_get_contents("http://ipinfo.io/{$_SERVER['REMOTE_ADDR']}");
+			$details = json_decode($response);
+			if($details->tor) {
+				$tor = "Login is disabled from TOR!";
+			}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?=COMPANY_NAME?></title>
+<style>@font-face {
+ font-family: Calibre;
+ src: url(https://siicrypto.com/css/calibre-regular.woff);
+}
+div{
+	padding:20px;
+	align:auto;
+	text-align:center;
+	background-color:#eee
+}
+body{
+	font-family: Calibre;
+	font-size:20px;
+	margin:10px;
+}
+td{
+	padding: 20px 20px 20px 20px;
+	text-align:left;
+}
+a{
+text-decoration: none
+}
+</style>
+</head>
+<body>
+<div style="border-radius: 10px;-webkit-border-radius: 10px;-moz-border-radius: 10px;">
+	<div style="background-color:black;border-radius: 10px;-webkit-border-radius: 10px;-moz-border-radius: 10px;">
+		<a href="https://siicrypto.com" target="_blank"><img src="https://siicrypto.com/img/logo.png" alt="SiiCrypto.com" text="SiiCrypto.com"/></a>
+	</div>
+	
+	<div>
+<p>Your deposit has been approved and deposited to your SiiCrypto Account.</p>
+		<table style="width:95%;	align:auto;	margin:auto;	border:0px;	background-color:white;">
 		<tr>
 			<td>Reference:</td>
 			<td><strong><?=$Transactions['Reference']?></strong></td>
@@ -19,10 +56,16 @@
 		</tr>		
 </table>
 
-
-<p>Thanks,<br>
-<?=NOREPLY?></p>
-
-<p>P.S. Please do not reply to this email. </p>
-	<p>If you did not initiate this action please contact SiiCrypto.com as soon as possible via support@SiiCrypto.com.</p>
+	</div>
+	<p>SiiCrypto is a REALLY safe crypto currency exchange.</p>
+	<p><a href="https://greencoinx.com">GreenCoinX.com</a> - <a href="https://xgcwallet.org">XGCWallet.org</a> - <a href="https://KYCGlobal.net">KYCGlobal.net</a></p>
+	<div style="padding:30px;font-size:10px">
+	<p>Please do not reply to this email. </p>
+	<p>This email was sent to you as you tried to register on <?=COMPANY_URL?> with the email address. 
+	If you did not register, then you can delete this email.</p>
 <p>We do not spam. </p>
+	</div>
+</div>
+</body>
+</html>
+
