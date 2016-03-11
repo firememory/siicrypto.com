d5b9d4e67a078023f85b6aab6b84ab57d441895c
Nilam Doctor <nilamdoc@gmail.com>
2016-03-11 22:55:09 +0530

    check

--- app/views/users/deposit.html.php
+++ app/views/users/deposit.html.php
@@ -4,7 +4,7 @@
 <p>Click to download the PDF directly from here <a href="/vanity/out/SiiCrypto-<?=$data['data']['Reference']?>.pdf" target="_blank">SiiCrypto-<?=$data['data']['Reference']?>.pdf</a></p>
 <p>Print, Sign and Upload the PDF document.</p>
 Once you upload "<strong>Declaration of Source of Funds (DSF)</strong>", we will send this document for SiiCrypto Admin Approval. Once it is approved, you can send the funds to the account.</p>
-<a href="/users/sendDeposit/<?=$data['data']['Reference']?>" class="btn btn-primary">Click to send to directly to your email to sign it</a>
+<!--<a href="/users/sendDeposit/<?=$data['data']['Reference']?>" class="btn btn-primary">Click to send to directly to your email to sign it</a>-->
 <p>&nbsp;</p>
 <a href="/ex/dashboard" class="btn btn-primary">Dashboard</a>
 <p>&nbsp;</p>
