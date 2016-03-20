ab1c84eb58f374888f709f6bd77df9f68946a243
Nilam Doctor <nilamdoc@gmail.com>
2016-03-20 08:05:04 +0530

    wnotify

--- app/extensions/command/WNotify.php
+++ app/extensions/command/WNotify.php
@@ -7,18 +7,20 @@ use app\extensions\action\Greencoin;
 
 class WNotify extends \lithium\console\Command {
  public function index($s=null) {
+		$transaction = Notifies::find("first",array(
+			'conditions'=>array(
+				'TransactionHash' => $s
+			)
+		));
 
-				
-							$t = Notifies::create();
-							$data = array(
-								'DateTime' => new \MongoDate(),
-								'TransactionHash' => $s,
-							);							
-							$t->save($data);
-
-				
-				
-			
+		if(count($transaction)==0){
+			$t = Notifies::create();
+			$data = array(
+				'DateTime' => new \MongoDate(),
+				'TransactionHash' => $s,
+			);							
+			$t->save($data);
 		}
+	}
 } 
 ?>
