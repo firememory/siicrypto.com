763c6b3447bd152d78e28f50f8a11bcb95e75be4
Nilam Doctor <nilamdoc@gmail.com>
2016-03-23 22:21:13 +0530

    analytics

--- app/views/elements/localbitcoins.html.php
+++ app/views/elements/localbitcoins.html.php
@@ -15,3 +15,14 @@ use lithium\storage\Session;
 <small style="font-size:9px">
 <?=$GLOBALS['userCountry']?>-<?=$GLOBALS['userState']?> - <?=$rate?></small>
 </div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75470134-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
