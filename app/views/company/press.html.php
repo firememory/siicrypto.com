e3a95d271ecd5d97b1838e0754b97397734f75bc
Nilam Doctor <nilamdoc@gmail.com>
2016-03-23 20:15:36 +0530

    press

--- app/views/company/press.html.php
+++ app/views/company/press.html.php
@@ -2,7 +2,17 @@
 
  
 <blockquote>
-<h3>GreenCoinX’s Affiliated Crypto Currency Exchange Launches</h3>
-<h5>SiiCrypto specializing in GreenCoinX is Commission Free and Safe</h5>
-<p><small>March 14, 2016</small></p>
+<h3>GREENCOINX’S AFFILIATED CRYPTO CURRENCY EXCHANGE SIICRYPTO LAUNCHES</h3>
+<h5>SiiCrypto specializing in GreenCoinX is Commission Free and Secure</h5>
+<p><small>Toronto, Ontario, March 23, 2016</small>GreenCoinX Inc, a subsidiary of GreenBank Capital Inc a Canadian
+public company (CSE: GBC and OTCMKTS: GRNBF) ("GreenCoinX") announces the launch of
+SiiCrypto its affiliated crypto currency exchange (www.SiiCrypto.com). SiiCrypto specializes in trading
+GreenCoinX, the world’s first crypto currency requiring user identification, and it charges no commission
+for trades. It is very secure to use as all client fiat funds are held by a third party, ILS Fiduciaries
+(Switzerland) Sarl, the Swiss branch of ILS World, a global provider of independent fiduciary services.
+Crypto currency held by SiiCrypto cannot be accessed without client approval. Unlike other online
+exchanges, SiiCrypto cannot use clients’ crypto currency or fiat funds as a float or for any other purpose.
+Every user of SiiCrypto is identified to the same "Know Your Customer" ("KYC") standards as exists in the banking system. <a href="/files/GreenCoinX press release SiiCrypto March 23  2016 v1.pdf" target="_blank">Read more...</a></p>
 </blockquote>
+
+
