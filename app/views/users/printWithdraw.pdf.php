3a24ebc32e23b65119f0f525f084d9c3cd13b53e
Nilam Doctor <nilamdoc@gmail.com>
2016-04-16 03:27:35 +0530

    withdraw

--- app/views/users/printWithdraw.pdf.php
+++ app/views/users/printWithdraw.pdf.php
@@ -55,18 +55,38 @@ $pdf->SetAutoPageBreak(true);
 					$html = $html.'<br><small>* '.$t("Withdrawal wire instructions Form to be signed and submitted by uploading to SiiCrypto for outward wire payment.")."</small>";
 				}
 				$html = $html."<br> Reference No: ".$data['Reference']."</div>";
-				$html = $html . "<div><small>Instructions: Please fully complete and sign this form before uploading. <br></small></div>";
+				$html = $html . "<div><small>";
+				$html = $html . "Instructions: Please fully complete and sign this form before uploading. ";
+				if($locale!='en'){
+					$html = $html . "<br>* ". $t("Instructions: Please fully complete and sign this form before uploading.");
+				}
+				$html = $html . "<br></small></div>";
 			
 				$html = $html . '<table border="1" cellspacing="0" cellpadding="2" style="border:1px solid black" >
 						<tr>
-							<th colspan="2" style="text-align:center;background-color:#CAFFFF">DETAILS OF YOUR ACCOUNT</th>
+							<th colspan="2" style="text-align:center;background-color:#CAFFFF">';
+							$html = $html . 'DETAILS OF YOUR ACCOUNT';
+							if($locale!='en'){
+								$html = $html . '<br><small>* '. $t('DETAILS OF YOUR ACCOUNT').'</small>';
+							}
+							$html = $html .'</th>
 						</tr>
 						<tr>
-							<th width="50%"><small>BANK ACCOUNT NAME:</small></th>
+							<th width="50%"><small>';
+							$html = $html .'BANK ACCOUNT NAME:';
+							if($locale!='en'){
+								$html = $html . '<br>* '. $t('BANK ACCOUNT NAME').':';
+							}
+							$html = $html .'</small></th>
 							<td><small>ILS FIDUCIARIES (SWITZERLAND) SARL</small></td>
 						</tr>
 						<tr>
-							<th><small>BANK ACCOUNT NUMBER:</small></th>
+							<th><small>';
+							$html = $html .'BANK ACCOUNT NUMBER:';
+							if($locale!='en'){
+								$html = $html .'<br>* '.$t('BANK ACCOUNT NUMBER').': ';
+							}
+							$html = $html .'</small></th>
 							<td>ACC '.$data['Currency'].'-100-070378-';
 								switch ($data['Currency']){
 										case "USD":
@@ -81,53 +101,98 @@ $pdf->SetAutoPageBreak(true);
 				$html = $html.'
 							</td>
 						</tr><tr>
-							<th><small>YOUR FULL NAME:</small></th>
+							<th><small>';
+							$html = $html.'YOUR FULL NAME:';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('YOUR FULL NAME').': ';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.$data['data']['AccountName'];
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>YOUR EMAIL ADDRESS</small></th>
+							<th><small>';
+							$html = $html.'YOUR EMAIL ADDRESS';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('YOUR EMAIL ADDRESS').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 					$html = $html.$data['data']['email'];							
 					$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>REFERENCE (Quote this reference number with wire)</small></th>
+							<th><small>';
+							$html = $html.'REFERENCE (Quote this reference number with wire)';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('REFERENCE (Quote this reference number with wire)');
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.$data['Reference'];							
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th colspan="2" style="text-align:center;background-color:#CAFFFF">AMOUNT OF YOUR OUTWARD WIRE PAYMENT</th>
+							<th colspan="2" style="text-align:center;background-color:#CAFFFF">';
+							$html = $html.'AMOUNT OF YOUR OUTWARD WIRE PAYMENT';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('AMOUNT OF YOUR OUTWARD WIRE PAYMENT');
+							}
+							$html = $html.'</th>
 						</tr>
 						<tr>
-							<th><small>CURRENCY</small></th>
+							<th><small>';
+							$html = $html.'CURRENCY';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('CURRENCY');
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.$data['Currency'];							
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>NET AMOUNT TO BE WIRED:</small></th>
+							<th><small>';
+							$html = $html.'NET AMOUNT TO BE WIRED:';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('NET AMOUNT TO BE WIRED').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.number_format($data['netAmount'],2);
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>AMOUNT: (Words)</small></th>
+							<th><small>';
+							$html = $html.'AMOUNT: (Words)';
+							if($locale!='en'){
+								$html = $html.'<br>* '.'AMOUNT: (Words)';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$Amount = ucwords($function->number_to_words($data['netAmount']))." ".$data['Currency']. " ONLY";				
 				$html = $html.$Amount;
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>VANTU BANK CHARGES DEDUCTED:</small></th>
+							<th><small>';
+							$html = $html.'VANTU BANK CHARGES DEDUCTED:';
+							if($locale!='en'){
+								$html = $html.'<br>*'.'VANTU BANK CHARGES DEDUCTED' .':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.number_format($data['VantuCharges'],2);
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>FIDUCIARY COST DEDUCTED</small></th>
+							<th><small>';
+							$html = $html.'FIDUCIARY COST DEDUCTED:';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('FIDUCIARY COST DEDUCTED').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.number_format($data['ILSCharges'],2);
 				$html = $html.'</td>
@@ -136,45 +201,101 @@ $pdf->SetAutoPageBreak(true);
 							<th colspan="2" style="text-align:center;background-color:#CAFFFF">DETAILS OF THE RECEIVING BANK FOR WIRE PAYMENT</th>
 						</tr>
 						<tr>
-							<th><small>FULL NAME OF RECEIVING PARTY</small></th>
+							<th><small>';
+							$html = $html.'FULL NAME OF RECEIVING PARTY';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('FULL NAME OF RECEIVING PARTY').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.$data['data']['AccountName'];							
 				$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>RECEIVING BANK ACCOUNT NUMBER</small></th>
+							<th><small>';
+							$html = $html.'RECEIVING BANK ACCOUNT NUMBER:';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('RECEIVING BANK ACCOUNT NUMBER').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 				$html = $html.$data['data']['AccountNumber'];							
 				$html = $html.'</td>
 						</tr>						<tr>
-							<th><small>RECEIVING BANK NAME</small></th>
+							<th><small>';
+							$html = $html.'RECEIVING BANK NAME:';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('RECEIVING BANK NAME').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 					$html = $html.$data['data']['BankName'];							
 					$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>RECEIVING BANK ADDRESS</small></th>
+							<th><small>';
+							$html = $html.'RECEIVING BANK ADDRESS';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('RECEIVING BANK ADDRESS').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 					$html = $html.$data['data']['BankAddress'];							
 					$html = $html.'</td>
 						</tr>
 						<tr>
-							<th><small>ORIGINAL SENDING BANK SWIFT CODE</small></th>
+							<th><small>';
+							$html = $html.'ORIGINAL SENDING BANK SWIFT CODE:';
+							if($locale!='en'){
+								$html = $html.'<br>* '.$t('ORIGINAL SENDING BANK SWIFT CODE').':';
+							}
+							$html = $html.'</small></th>
 							<td>';
 					$html = $html.$data['data']['SwiftCode'];							
 					$html = $html.'</td>
 						</tr>
-						</table>
-						<div><small>
-				<p>I/We confirm that the funds requested to be withdrawn from my SiiCrypto account will only be transmitted to my/our own bank account. I/We understand that under the requirements of Vanuatu’s Anti-Money Laundering & Counter-Terrorism Financing Act No. 13 of 2014, Regulations made thereunder and Vantu Bank’s and National Bank of Vanuatu’s respective AML/CTF Compliance Manuals as currently in force, your policy may require both banks to be satisfied as to the source of this payment before accepting any outward wire transfer and that my/our transfer(s) may be held pending or returned in the absence of such confirmation.</p>
-				<p>I/We declare that the above information is true and correct.</p></small></div>';
+						</table>';
+						if($locale!='en'){
+								$pdf->writeHTML($html, true, 0, true, 0);
+								$pdf->AddPage();
+								$pdf->SetXY(20,20,false);
+								$html = '';
+							}
+						
+						$html = $html.'<div><small>
+				<p>';
+				$html = $html.'I/We confirm that the funds requested to be withdrawn from my SiiCrypto account will only be transmitted to my/our own bank account. I/We understand that under the requirements of Vanuatu’s Anti-Money Laundering & Counter-Terrorism Financing Act No. 13 of 2014, Regulations made thereunder and Vantu Bank’s and National Bank of Vanuatu’s respective AML/CTF Compliance Manuals as currently in force, your policy may require both banks to be satisfied as to the source of this payment before accepting any outward wire transfer and that my/our transfer(s) may be held pending or returned in the absence of such confirmation.';
+				if($locale!='en'){
+					$html = $html.'<br>* '.$t('I/We confirm that the funds requested to be withdrawn from my SiiCrypto account will only be transmitted to my/our own bank account. I/We understand that under the requirements of Vanuatu’s Anti-Money Laundering & Counter-Terrorism Financing Act No. 13 of 2014, Regulations made thereunder and Vantu Bank’s and National Bank of Vanuatu’s respective AML/CTF Compliance Manuals as currently in force, your policy may require both banks to be satisfied as to the source of this payment before accepting any outward wire transfer and that my/our transfer(s) may be held pending or returned in the absence of such confirmation.');
+				}
+				$html = $html.'</p>
+				<p>';
+				$html = $html.'I/We declare that the above information is true and correct.';
+				if($locale!='en'){
+					$html = $html.'<br>* '.$t('I/We declare that the above information is true and correct.');
+				}
+				'</p></small></div>';
 				$html = $html . 'Date: '.gmdate('Y-M-d H:i:s',time());
-				$html = $html . '<div style="text-align:right;"><u>'.$data['data']['AccountName'].'</u><br>
-				Account Holder\'s Name<br></div>';
+				$html = $html . '<div style="text-align:right;"><u>'.$data['data']['AccountName'].'</u><br>';
+				$html = $html . 'Account Holder\'s Name';
+				if($locale!='en'){
+					$html = $html . '<br>* '.$t('Account Holder\'s Name');
+				}
+				$html = $html . '<br></div>';
 				$html = $html . '<p></p>';
-				$html = $html . '<div style="text-align:right">Please sign here:______________________________________________<br><u>'.$data['data']['AccountName'].'</u><br>
-				Account Holder\'s Signature
-				</div>';
+				$html = $html . '<div style="text-align:right">';
+				$html = $html . 'Please sign here:';
+				if($locale!='en'){
+					$html = $html . '<br>* '. $t('Please sign here').':';
+					$html = $html . '______________________________________________<br><u>'.$data['data']['AccountName'].'</u><br>';
+				}
+				$html = $html . '______________________________________________<br><u>'.$data['data']['AccountName'].'</u><br>';
+				
+				$html = $html . 'Account Holder\'s Signature';
+				if($locale!='en'){
+					$html = $html .'<br>* '.$t( 'Account Holder\'s Signature');
+				}
+				$html = $html . '</div>';
 				$html = $html .'IP: '. $_SERVER['REMOTE_ADDR'];
 				$pdf->writeHTML($html, true, 0, true, 0);
 
