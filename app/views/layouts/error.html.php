99e02d0481ce71e18812be34ea088547224a1746
Nilam Doctor <nilamdoc@gmail.com>
2017-01-25 17:44:41 +0530

    siicrypto

--- app/views/layouts/error.html.php
+++ app/views/layouts/error.html.php
@@ -1,2 +1,270 @@
-<?php   echo $this->content(); ?>
-Error in URL... <a href="/login"> Login again...</a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="SiiCrypto, SiiCrypto homepage, bitcoin, buy bitcoin, sell bitcoin, litecoin, cryptocurrency, exchange, gbp, UK, btc, bitcoin exchange, digital currency exchange, greencoin">	
+		<meta name="description" content="A digital currency exchange where you can buy or sell Bitcoin and other Cryptocurrencies with ease via multiple funding methods to suit your needs.">		
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+
+
+		<title>SiiCrypto Exchange | SiiCrypto | Digital Currency Exchange - Buy & Sell Bitcoin</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
+				<link href="/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="/bootstrap/css/dashboard.css?v=47991976" rel="stylesheet">
+<style type="text/css">
+body {
+	padding-top: 40px;
+	font-family: 'Open Sans', sans-serif;
+}
+</style>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800italic' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"> 
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+<!--		<script src="/bootstrap/js/jquery.js"></script> -->
+	<script src="/bootstrap/js/bootstrap-datepicker.js"></script>	
+	</head>
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<body  onLoad="CheckServer();" >
+
+    <div style="padding-right:0;" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div  style="padding-right:0; padding-left:0;" class="container-fluid">
+						
+<div class="navbar-header">
+	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="/en/"><img src="/img/logo.png" alt="SiiCrypto.com" title="SII Crypto"></a>
+</div> <!-- navbar-header-->
+<div class="navbar-collapse collapse">
+		
+				
+	<ul class="nav navbar-nav navbar-right">
+		<li><a href='#' class='dropdown-toggle' data-toggle='dropdown' >Lang: en		 <i class='glyphicon glyphicon-chevron-down'></i>
+		</a>
+			<ul  class="dropdown-menu">
+			<li style="font-size:13px;"><a href="#" onclick="ChangeLanguage('en','/');"><small>English</small></a></li>	
+			<li style="font-size:13px;"><a href="#" onclick="ChangeLanguage('de','/');"><small>Deutsch</small></a></li>
+			<li style="font-size:13px;"><a href="#" onclick="ChangeLanguage('es','/');"><small>Español</small></a></li>
+			<li style="font-size:13px;"><a href="#" onclick="ChangeLanguage('hi','/');"><small>हिन्दी</small></a></li>
+			</ul>
+		</li>
+					<li ><a href='#' class='dropdown-toggle' data-toggle='dropdown' >
+			Company <i class='glyphicon glyphicon-chevron-down'></i>
+				</a>
+			
+				<ul class="dropdown-menu">
+					<li style="font-size:13px;"><a href="/en/company/aboutus">About</a></li>	
+					<li style="font-size:13px;"><a href="/en/company/howitworks">How it works</a></li>	
+					<li style="font-size:13px;"><a href="/en/company/funding">Funding</a></li>
+					<li style="font-size:13px;"><a href="/en/company/security">Security &amp; Risk</a></li>	
+					<li style="font-size:13px;"><a href="/en/company/verification">Verification</a></li>						
+					<li style="font-size:13px;"><a href="/en/company/privacy">Privacy &amp; Terms</a></li>		
+					<li style="font-size:13px;"><a href="/en/company/press">Press</a></li>		
+				</ul>
+				</li>
+			<li><a href="/en/login">Login</a></li>
+			<li><a href="/en/users/signup">Register&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+							
+		</ul>
+</div> <!-- navbar-collapse -->
+<!-- Modal on default page-->		
+      </div>  <!-- container-fluid -->
+    </div> <!-- navbar-fixed-top -->
+    <div class="container-fluid">
+      <div class="row">
+									<div class="col-sm-4 col-md-3 sidebar">
+						<ul class="nav nav-sidebar">
+							<li>
+								<div style="padding-bottom:10px;font-size:15px;text-align:center">
+<!--								<a href="#" onclick="ChangeLanguage('en','/');" style="border-bottom:double">
+								<img src="/img/Flags/en.png" width="75"></a> 
+								<a href="#" onclick="ChangeLanguage('de','/');" style="border-bottom:double">
+								<img src="/img/Flags/de.png" width="75"></a>
+-->								
+									<a href="#" onclick="ChangeLanguage('en','/');" style="border-bottom:double"><small>English</small></a>
+									<a href="#" onclick="ChangeLanguage('de','/');" style="border-bottom:double"><small>Deutsch</small></a>
+									<a href="#" onclick="ChangeLanguage('es','/');" style="border-bottom:double"><small>Español</small></a>
+									<a href="#" onclick="ChangeLanguage('hi','/');" style="border-bottom:double"><small>हिन्दी</small></a>
+								</div>
+							</li>
+							
+							<li class="active"><a href="#"> <i class="glyphicon glyphicon-th-list"></i> Trades</a></li>
+							<li>
+<a href="/en/ex/x/btc_xgc" class="list-group-subitem">
+	<i class="glyphicon glyphicon-caret-right"></i>BTC/XGC <span class="badge btn-success pull-right">365.1273</span>
+</a>				
+</li>
+<li>
+<a href="/en/ex/x/xgc_cad" class="list-group-subitem">
+	<i class="glyphicon glyphicon-caret-right"></i>XGC/CAD <span class="badge btn-success pull-right">1.5267</span>
+</a>				
+</li>
+<li>
+<a href="/en/ex/x/xgc_gbp" class="list-group-subitem">
+	<i class="glyphicon glyphicon-caret-right"></i>XGC/GBP <span class="badge btn-success pull-right">0.0000</span>
+</a>				
+</li>
+<li>
+<a href="/en/ex/x/xgc_usd" class="list-group-subitem">
+	<i class="glyphicon glyphicon-caret-right"></i>XGC/USD <span class="badge btn-success pull-right">1.1500</span>
+</a>				
+</li>
+<li>
+<a href="/en/ex/x/xgc_eur" class="list-group-subitem">
+	<i class="glyphicon glyphicon-caret-right"></i>XGC/EUR <span class="badge btn-success pull-right">1.2000</span>
+</a>				
+</li>
+<!--
+<div class="comingsoon"><h3>Coming soon...</h3>
+<p><img src="/img/Dollar.png"> USD</p>
+<p><img src="/img/Euro.png"> EUR</p>
+<p><img src="/img/Pound.png"> GBP</p>
+<p><img src="/img/CDollar.png"> CAD</p>
+</div>
+-->		
+													</ul>
+												<ul class="nav nav-sidebar">
+						<li class="active"><a href="/en/users/signup">Register &amp; Open an account</a></li>
+						</ul>
+						<small style="letter-spacing: 0px;">
+<p>SiiCrypto first time users need to open an online GreenCoinX wallet at <a href="https://xgcwallet.org" target="_blank">www.xgcwallet.org</a></p>
+<p>Users will complete an online &quot;Know Your Client&quot; verification process which only takes a few minutes.</p>
+<p><strong>SiiCrypto clients fiat funds are held by ILS Fiduciaries (Switzerland) Sarl the Swiss branch of ILS World, a global provider of independent fiduciary services.</strong></p>
+<p><strong>SiiCrypto clients GreenCoinX and Bitcoin cannot be accessed without client approval.</strong></p>
+<p>The promise of crypto currency was always the fast and inexpensive transfer of funds worldwide, while bypassing the banking system.Until GreenCoinX this has been impeded by identity concerns. Now GreenCoinX has made this promise a reality by its KYC capability.</p>
+</small>
+					</div> <!-- sidebar-->
+					
+												<div class="col-sm-8 col-sm-offset-4 col-md-9 col-md-offset-3 main">
+								
+					<div class="main">
+		<h2 style="text-align:center;margin-top:-10px"><p>THE SAFE AND SECURE CRYPTO CURRENCY EXCHANGE WITH</p><p> <span style="color:green"> NO COMMISSION CHARGES</span></p>
+
+<p>BUY/SELL <span style="color:green">GREENCOINX</span> FOR EURO, STERLING, US DOLLAR, CANADIAN DOLLAR OR BITCOIN</p>
+		
+		</h2>
+		
+		<p style="text-align:center;font-size:18px"><strong>SiiCrypto facilitates trading in GreenCoinX the world&#039;s first crypto currency that requires user identification</strong></p>
+</div><div class="main">
+		<div class="row placeholders">
+						<div class="col-xs-3 col-sm-3  placeholder">
+						<a href="/en/ex/x/xgc_eur"><img src="/img/xgc_eur.png" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>XGC/EUR</h4>
+						<small>Min: 1.2000<br> Max: 1.2000<br> Last: 1.2000						</small>
+						</a>
+				</div>
+									<div class="col-xs-3 col-sm-3  placeholder">
+						<a href="/en/ex/x/xgc_gbp"><img src="/img/xgc_gbp.png" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>XGC/GBP</h4>
+						<small>						</small>
+						</a>
+				</div>
+									<div class="col-xs-3 col-sm-3  placeholder">
+						<a href="/en/ex/x/xgc_usd"><img src="/img/xgc_usd.png" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>XGC/USD</h4>
+						<small>Min: 1.1500<br> Max: 1.1500<br> Last: 1.1500Min: 1.1500<br> Max: 1.1500<br> Last: 1.1500						</small>
+						</a>
+				</div>
+									<div class="col-xs-3 col-sm-3  placeholder">
+						<a href="/en/ex/x/xgc_cad"><img src="/img/xgc_cad.png" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>XGC/CAD</h4>
+						<small>Min: 1.5267<br> Max: 1.5267<br> Last: 1.5267						</small>
+						</a>
+				</div>
+							</div>
+		<div class="row placeholders">
+				<div class="col-xs-6 col-sm-12  placeholder">
+				</div>
+				<div class="col-xs-6 col-sm-12  placeholder">
+						<a href="/en/ex/x/btc_xgc"><img src="/img/btc_xgc.png" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>BTC/XGC</h4>
+						<small>
+						Min: 365.1273<br> Max: 365.1273<br> Last: 365.1273						</small>
+						</a>
+				</div>
+				<div class="col-xs-6 col-sm-12  placeholder">
+				</div>
+		</div>
+		<div class="row placeholders">
+				</div>
+	</div>
+
+<div class="col-xs-12 col-sm-4">
+		<!-- <a class="twitter-timeline"  href="https://twitter.com/SIICrypto"  data-widget-id="aaaaaaaaaaaaaaaaaaaa">Tweets by @SIICrypto</a> -->
+</div>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>					<div class="footer" style="border-bottom:double">
+							<div class="row container-fluid">
+<small style="font-size:9px">
+-</small>
+</div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75470134-1', 'auto');
+  ga('send', 'pageview');
+
+</script>	
+								
+					</div>	<!-- footer -->
+				</div> <!-- main -->					
+			</div> <!-- row-->
+			
+		</div> <!-- container-fluid -->
+		
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+		
+	<script src="/js/main.js?v=74832281"></script>
+	
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/bootstrap/js/docs.min.js"></script>
+  </body>
+</html>
+<script type="text/javascript">
+$(function() {
+ $('.tooltip-x').tooltip();
+ $('.tooltip-y').tooltip(); 
+ $("input:text:visible:first").focus();
+});
+</script>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">- Warning</h4>
+      </div>
+      <div class="modal-body">
+												<h3>TRADING INFORMATION - NON USA </h3>
+							<p class="alert alert-danger">We regret to advise you that due to regulatory issues residents of some countries are excluded from trading on SiiCrypto at this time.<br><br>
+							Your country requires additional licensing which we intend to complete as soon as possible, or it has been excluded for other regulatory reasons.<br><br>
+								We will inform you when this issue has been resolved. Thank you for your patience.</p>
+						      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
